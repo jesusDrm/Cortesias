4b53d99ca675d2532865896ba9f20f58bdbacdb3
Diego <diego.nwm.117@gmail.com>
2024-08-23 11:21:25 -0600

    reemplazo de base de datos

--- pasaportes.sql
+++ pasaportes.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 21-08-2024 a las 18:39:46
--- Versión del servidor: 5.7.15-log
--- Versión de PHP: 5.6.26
+-- Tiempo de generación: 23-08-2024 a las 17:17:29
+-- Versión del servidor: 8.0.17
+-- Versión de PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,10 +30,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `historial` (
   `ID` int(11) NOT NULL,
-  `Nombre y codigo` varchar(50) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Clave` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Fecha` date NOT NULL,
-  `Rango de folios` varchar(30) COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `NumeroCortesias` int(100) NOT NULL,
+  `Clave de rango inicial` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Clave de rango final` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -41,9 +46,9 @@ CREATE TABLE `historial` (
 
 CREATE TABLE `nombres` (
   `ID` int(11) NOT NULL,
-  `Nombre` varchar(40) COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Clave` varchar(40) COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Clave` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `nombres`
@@ -71,7 +76,7 @@ CREATE TABLE `posicion` (
   `ejey_texto` int(8) NOT NULL,
   `area_qr` int(8) NOT NULL,
   `tam_texto` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `posicion`
@@ -79,6 +84,25 @@ CREATE TABLE `posicion` (
 
 INSERT INTO `posicion` (`ejey_qr`, `ejex_qr`, `ejex_texto`, `ejey_texto`, `area_qr`, `tam_texto`) VALUES
 (595, 82, 130, 875, 260, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prueba`
+--
+
+CREATE TABLE `prueba` (
+  `id` int(11) NOT NULL,
+  `Cortesias` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `prueba`
+--
+
+INSERT INTO `prueba` (`id`, `Cortesias`) VALUES
+(1, 'pswas123'),
+(2, 'Pruebemos');
 
 --
 -- Índices para tablas volcadas
@@ -97,6 +121,23 @@ ALTER TABLE `historial`
 ALTER TABLE `nombres`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `ID` (`ID`);
+
+--
+-- Indices de la tabla `prueba`
+--
+ALTER TABLE `prueba`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `prueba`
+--
+ALTER TABLE `prueba`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
